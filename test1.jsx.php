050cbae2068faadc28b079a55f335a8aff763161
Sergiu Toderascu <sergiu.toderascu@gmail.com>
2015-12-12 20:31:34 +0200

    Create test1.jsx.php

---
+++ test1.jsx.php
@@ -0,0 +1,81 @@
+<?php
+
+    # Compiled by Junx
+
+    # 2014-09-24T05:03:39+03:00
+    # Numbers: 0.036 - 0.038sec / 1.50MB
+
+    use /db/mongo;
+    use /google/anlytics/track;
+
+    class hello extends JunxApp {
+
+        public $misc = array();
+        public $util = array();
+        public $time = array();
+        public $appsettings = array();
+        public $name = array();
+        public $alpha = array();
+        public $ulelea = array();
+        public $barssssss = array();
+        public $btz = array();
+        public $db = array();
+        public $api = array();
+        public $indent_objs = array();
+
+        function __construct( $arg = array('param' => '1') ) {
+
+
+            $this->indent_objs = array(0 => 'un text care are loc de lista in array', 1 => 'Some another text that can be used', 2 => '\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'', 3 => '                                                            ', 4 => '   jjjjjjj     jj      jj      jj jjjjjjj      jj     jj    ', 5 => '       jj      jj      jj      jjj     jj       jj   jj     ', 6 => '       jj      jj      jj      jj      jj        jj jj      ', 7 => '       jj      jj      jj      jj      jj         jjj       ', 8 => '       jj      jj      jj      jj      jj        jjjj       ', 9 => '       jj      jj      jj      jj      jj       jj  jj      ', 10 => '       jj      jj      jj      jj      jj      jj    jj     ', 11 => '   jjjjj        jjjjjjjj       jj      jj     jj      jj    ', 12 => '                                                            ', 13 => '\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'', 'Luni' => array(0 => 'Dimineata_pe_racoare', 'e' => array('d' => array('c' => array('b' => 'a'))), 'd' => array('c' => array('b' => 'a')), 'c' => array('b' => 'a'), 'b' => 'a', 1 => 'a', 'bobobo' => array('J1086' => array(0 => 'system', 1 => 'date')), 'asds' => 'nnnnn', 'bumbumslsls' => array(0 => 'testing', 1 => 'inline', 2 => 'listing', 3 => 'items', 4 => 'as', 5 => 'many', 6 => 'as', 7 => 'possible'), 'keyaa' => 'bluas', 'line' => array(0 => 'a', 'b' => array('c' => array('d' => array('e' => array('f' => array('g' => array('h' => array('k' => array('l' => array('m' => array('n' => array(0 => 1, 1 => array())))))))))))), 'test' => array('wowowowo' => array('kksks' => array('J1086' => array(0 => 'db', 1 => array('app' => NULL, 'call' => 'select1', 'jxo' => array('c' => 'd', 'f' => array(0 => '1', 1 => '2', 2 => '3'), 'a' => 'b')))), 'dsfdfdf' => array(0 => 'sdfdf', 1 => 'fgaalkjk', 2 => 'dfdkjdfskj'), 'jsjsjsj' => array('foofof' => 'blabla', 'or' => array('here' => 'hara'), 'baz' => 'whah', 'a' => 'b', 'arra' => array(0 => 'a', 1 => 'b', 2 => 'c')), 'foo' => array('bar' => array('a' => 'b'))))), 'x' => array('y' => array('a' => array('b' => array('c' => true)))), 'Miercuri' => array('user' => 'root', 'pass' => 'Hls360s90', 'host' => 'localhost'), 17 => 'Joi', 'Vineri' => array(0 => 'una', 1 => 'dou', 2 => 'trei', 'patru' => 'cinci', 3 => 'sase'), 19 => 'Sambata');
+
+            $this->api = array('J1086' => array(0 => 'system', 1 => 'api'));
+
+            $this->db = array('J1086' => array(0 => 'db', 1 => array('app' => NULL, 'call' => 'mongo', 'jxo' => array('user' => 'root', 'pass' => 'Hls360s90', 'host' => 'localhost'))));
+
+            $this->btz = array('J1086' => array(0 => 'store', 1 => 'btzsoft', 2 => 'someapp2'));
+            doit(NULL);
+
+            $this->barssssss = array('J1086' => array(0 => 'lib', 1 => array('app' => 'others', 'call' => 'myapp', 'jxo' => array('foo' => 'bar'))));
+            select1(array('c' => 'd', 'f' => array(0 => '1', 1 => '2', 2 => '3'), 'a' => 'b'));
+
+            $this->ulelea = array('J1069' => array('app' => 'db', 'call' => 'select2', 'jxo' => array(0 => '1', 1 => '2', 2 => array('foo' => array('b' => '4', 'c' => array(0 => '1', 1 => '2', 2 => '3'), 'a' => '3')), 3 => '33', 4 => '333', 5 => array(0 => 4, 1 => 5, 2 => 6, 3 => 7, 4 => 8, 5 => 9, 6 => 10, 7 => 11, 8 => 12, 9 => 13, 10 => 14, 11 => 15))));
+
+            $this->alpha = array(0 => 'a', 1 => 'b', 2 => 'c', 3 => 'd', 4 => 'e', 5 => 'f', 6 => 'g', 7 => 'h', 8 => 'i', 9 => 'j', 10 => 'k', 11 => 'l', 12 => 'm', 13 => 'n', 14 => 'o', 15 => 'p', 16 => 'q', 17 => 'r', 18 => 's', 19 => 't', 20 => 'u', 21 => 'v', 22 => 'w', 23 => 'x', 24 => 'y', 25 => 'z');
+
+            $this->name = 'andrei';
+
+            $this->app['settings'] = array('db' => array('host' => 'localhost', 'user' => 'root', 'pass' => 'Hls360s90', 'engine' => array('J1086' => array(0 => 'db', 1 => 'mongo'))), 'timezone' => 'MALDOVA');
+
+            $this->time = array('foo' => array('J1086' => array(0 => 'system', 1 => 'time')), 'test' => '1');
+
+            $this->util = array('J1086' => array(0 => 'common', 1 => 'utils'));
+
+            $this->misc = array('J1086' => array(0 => 'store', 1 => 'btzsoft', 2 => 'someapp'));
+            upsert(array('foo' => 'bar', 'id' => '1'));
+        }
+
+
+        public function video( $arg = NULL ) {
+
+
+            echo('<ul>');
+
+                echo('<li>');
+                    echo('video.title');
+                echo('</li>');
+
+            echo('</ul>');
+
+        }
+
+
+        public function test( $arg = array('name' => 'Sergiu', 'param' => '1') ) {
+
+            select(array('id' => 'foo'));
+
+            if (param == 1) {
+                print('app.settings.db');
+            }
+
+        }
+
