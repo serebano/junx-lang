290fc0f677b0165142a40cca7c893cf8108ff4c5
Sergiu Toderascu <sergiu.toderascu@gmail.com>
2015-12-12 20:28:00 +0200

    Create test1.jsx

---
+++ test1.jsx
@@ -0,0 +1,178 @@
+
+# app (route: ./): (h1).on(click, alert mata) Hello World!
+
+app hello(param: 1)
+
+    indent_objs:
+    
+        "un text care are loc de lista in array"
+        "Some another text that can be used"
+        
+        "''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''"
+        "                                                            "
+        "   jjjjjjj     jj      jj      jj jjjjjjj      jj     jj    "
+        "       jj      jj      jj      jjj     jj       jj   jj     "
+        "       jj      jj      jj      jj      jj        jj jj      "
+        "       jj      jj      jj      jj      jj         jjj       "
+        "       jj      jj      jj      jj      jj        jjjj       "
+        "       jj      jj      jj      jj      jj       jj  jj      "
+        "       jj      jj      jj      jj      jj      jj    jj     "
+        "   jjjjj        jjjjjjjj       jj      jj     jj      jj    "
+        "                                                            "
+        "''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''"
+            
+        Luni
+        
+            Dimineata_pe_racoare
+            
+                    blabla
+                
+                    samelevel
+                    
+                    
+                        a
+                        a: b
+                        a: b: c
+                        a: b: c: d
+                        a: b: c: d: e
+            e: d: c: b: a
+               d: c: b: a
+                  c: b: a
+                     b: a 
+                        a
+                        
+                        
+                        bobobo
+                            
+                            @system.date
+                        
+                        asds: nnnnn
+                        
+                        bumbumslsls
+                        
+                            testing
+                            inline
+                            listing, items, as, many, as, possible
+                        
+                        keyaa: bluas
+                        
+                        line
+                        
+                            a
+                            b
+                                c
+                                    d
+                                        e
+                                            f
+                                                g
+                                                    h
+                                                        k
+                                                            l
+                                                                m
+                                                                    n
+                                                                        1
+                        
+            
+            test
+            
+                wowowowo
+                        
+                    kksks
+                    
+                        @db.select1({a: b, c: d, f: [1, 2, 3]})
+                        
+                    dsfdfdf
+                    
+                        sdfdf
+                        fgaalkjk
+                        dfdkjdfskj
+                    
+                    jsjsjsj
+                    
+                        foofof: blabla
+                        baz: whah, or: here: hara
+                        a: b
+                        arra: a..c
+                    
+                    foo: bar: a: b
+        
+        x
+            y   
+                a
+                    b
+                        c
+                            true
+                            
+        x: y: a: b: c: true
+        
+        
+        Miercuri
+            host: localhost, user: root, pass: Hls360s90
+        
+        Joi
+        
+        Vineri
+            una
+            dou
+            trei
+            patru: cinci
+            sase
+        
+        Sambata
+        
+    api: @system.api
+    
+    @db.mysql
+    
+    
+    dbs:
+    
+        @db.mongo
+        @db.mysql
+        
+    
+    
+    db.select()
+    
+    @db.mongo(host: localhost, user: root, pass: xxx) as db
+    
+    @google.anlytics(97983893).track()
+        
+    @store.btzsoft.someapp2 as btz
+    
+    btz.doit()
+    
+    @lib.others.myapp(foo: bar) as barssssss
+    
+    db.select1({a: b, c: d, f: [1, 2, 3]})
+    db.select2(1, 2, foo: a: 3, b: 4, c: [1, 2, 3], 33, 333, 4..15) as ulelea
+    
+    a..z as alpha
+    
+    andrei as name
+    
+    app.settings: timezone: MOLDOVA, db: engine: @db.mongo, host: localhost, user: root, pass: Hls360s90
+    
+    time: test: 1, foo: @system.time
+    util: @common.utils
+    misc: @store.btzsoft.someapp
+    
+    @db.mongo(user: test, pass: test, persistent: true)
+    
+    mongo.upsert(id: 1, foo: bar)
+            
+    def video()
+        (ul)
+            (li)
+                video.title
+            
+            
+    def test(param: 1, name: Sergiu)
+    
+        mongo.select(id: foo)
+        
+        if param == 1
+            print(app.settings.db)
+
+
+ 
